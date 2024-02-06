a1c3ca0331a96fc30567420473fde8e0ccd2fbc0
thanhtrung321-prog <tvo30879@hmail.com>
2024-02-06 19:21:43 +0700

    xong phần thông tin

--- frontend/src/page/Inforusers.js
+++ frontend/src/page/Inforusers.js
@@ -1,39 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Inforusers = () => {
   const userData = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+
   return (
-    <div className="flex bg-slate-300 w-full h-[100vh] m-auto items-center p-8">
-      <div className="bg-blue-300 p-4 rounded-md flex ">
-        <div className="p-4 rounded-md mr-8 text-center ">
-          <h3 className="text-2xl mb-2">Hình ảnh:</h3>
+    <div className="container mx-auto flex flex-col items-center justify-center px-4 py-8 h-screen">
+      <div className="flex flex-col bg-blue-300 rounded-lg shadow-md w-full md:max-w-md mx-auto">
+        <div className="flex items-center justify-center px-4 py-4">
+          <h3 className="text-2xl font-bold">Hình ảnh:</h3>
+        </div>
+        <div className="flex flex-col items-center justify-center px-4 py-4">
           <img
             src={userData.image}
-            id="image-user"
-            className="image-user w-20 h-20 rounded-full"
+            id="image-user2"
+            className="w-60 h-60 md:w-full md:h-auto object-cover md:rounded-none"
             alt=""
           />
+          <label className="mt-4 bg-slate-300 rounded-lg px-4 py-2 text-center cursor-pointer hover:bg-slate-400">
+            <h4 className="text-base font-medium">Cập nhật hình ảnh</h4>
+            <input type="file" id="profileimage" className="hidden" />
+          </label>
         </div>
-        <div className="flex flex-col">
-          <div className="flex items-center text-xl mb-2">
+        <div className="px-4 py-4 space-y-4 text-center">
+          <div className="flex flex-col items-center">
             <span className="font-bold mr-2">Tên chủ sở hữu tài khoản:</span>
-            <p>{userData.firstName}</p>
+            <p className="text-lg">{userData.firstName}</p>
           </div>
-          <div className="flex items-center text-xl mb-2">
+          <div className="flex flex-col items-center">
             <span className="font-bold mr-2">Tên Đệm:</span>
-            <p>{userData.lastName}</p>
+            <p className="text-lg">{userData.lastName}</p>
           </div>
-          <div className="flex items-center text-xl">
+          <div className="flex flex-col items-center">
             <span className="font-bold mr-2">Email:</span>
-            <p>trung@gmail.com</p>
+            <p className="text-lg">{userData.email}</p>
           </div>
         </div>
-        <div className="flex flex-col ml-5">
-          <p className="font-bold mr-2 bg-slate-500 rounded text-white cursor-pointer p-2 hover:bg-slate-700">
+        <div className="flex items-center justify-center px-4 py-4">
+          <button className="bg-slate-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-slate-600">
             Đổi mật khẩu
-          </p>
+          </button>
         </div>
       </div>
     </div>
